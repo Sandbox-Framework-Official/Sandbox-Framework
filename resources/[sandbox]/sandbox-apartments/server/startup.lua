2d9c337c6e4b812efac6615583f1c54cce8d12ec
Luke <mysticz2024@outlook.com>
2025-04-24 02:16:48 +0100

    apartment poly changes

--- resources/[sandbox]/sandbox-apartments/server/startup.lua
+++ resources/[sandbox]/sandbox-apartments/server/startup.lua
@@ -1,73 +1,73 @@
 _aptData = {
 	{
-		name = "Richard's Tower",
+		name = "Boring Tower",
 		invEntity = 13,
-		coords = vector3(-1274.351, 315.095, 64.512),
-		heading = 153.319,
+		coords = vector3(-481.83, -690.74, 33.21),
+		heading = 96.566,
 		length = 8.4,
 		width = 4.0,
 		options = {
 			heading = 0,
 			--debugPoly=true,
-			minZ = 63.21,
-			maxZ = 67.21,
+			minZ = 32.21,
+			maxZ = 36.21
 		},
 		interior = {
 			wakeup = {
-				x = 285.733,
-				y = -928.362,
-				z = -23.954,
-				h = 268.463,
+				x = -459.262,
+				y = -682.010,
+				z = 65.894,
+				h = 90.0,
 			},
 			spawn = {
-				x = 291.069,
-				y = -924.995,
-				z = -22.995,
-				h = 174.465,
+				x = -461.324,
+				y = -685.468,
+				z = 66.856,
+				h = 356.578,
 			},
 			locations = {
 				exit = {
-					coords = vector3(291.04, -924.59, -23.0),
-					length = 1.0,
-					width = 1.6,
+					coords = vector3(-461.21, -686.15, 66.86),
+					length = 0.8,
+					width = 2.6,
 					options = {
 						heading = 0,
 						--debugPoly=true,
-						minZ = -24.0,
-						maxZ = -21.4
+						minZ = 65.86,
+						maxZ = 68.46
 					},
 				},
 				wardrobe = {
-					coords = vector3(287.15, -922.39, -23.0),
+					coords = vector3(-461.71, -682.72, 66.86),
 					length = 1.0,
 					width = 2.0,
 					options = {
 						heading = 270,
 						--debugPoly=true,
-						minZ = -24.0,
-						maxZ = -21.6
+						minZ = 65.86,
+						maxZ = 68.06
 					},
 				},
 				stash = {
-					coords = vector3(288.89, -924.91, -23.0),
-					length = 0.8,
-					width = 2.4,
+					coords = vector3(-456.63, -682.55, 66.89),
+					length = 2.4,
+					width = 1.0,
 					options = {
 						heading = 0,
 						--debugPoly=true,
-						minZ = -24.0,
-						maxZ = -22.0
+						minZ = 65.89,
+						maxZ = 67.29
 					},
 				},
 				logout = {
-					coords = vector3(284.92, -928.52, -23.0),
-					length = 2.6,
-					width = 2.2,
+					coords = vector3(-458.43, -681.88, 66.89),
+					length =3.0,
+					width = 2.8,
 					options = {
 						heading = 0,
 						--debugPoly=true,
-						minZ = -24.0,
-						maxZ = -22.8
+						minZ = 65.89,
+						maxZ = 67.09
 					},
 				},
 			},
