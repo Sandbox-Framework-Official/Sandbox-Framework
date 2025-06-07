ba81522659cc368fa2a14c3a24a0081ebacb8ba2
TehGuardian <72229591+TehGuardian@users.noreply.github.com>
2025-06-07 13:02:15 -0400

    Cleanup Bike Tricks

--- resources/[standalone]/biketricks/fxmanifest.lua
+++ resources/[standalone]/biketricks/fxmanifest.lua
@@ -1,11 +1,5 @@
 fx_version 'cerulean'
 
--- description 'YBN Bike Tricks Edited By 8x for free updates join discord https://discord.gg/fandgwjNUm'
--- author 'Prenses Baran#0001'
-
-author 'nerxa'
-description 'pick you bike up, homie.'
-
 game 'gta5'
 
 client_scripts {
